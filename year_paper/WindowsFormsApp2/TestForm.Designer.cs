64b2ac91e7eafd5f195abe4728f7007f7703e404
Pasha Eagle <36258180+PashaEagle@users.noreply.github.com>
2019-04-15 07:03:39 +0300

    Small features added

--- year_paper/WindowsFormsApp2/TestForm.Designer.cs
+++ year_paper/WindowsFormsApp2/TestForm.Designer.cs
@@ -29,23 +29,24 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.button4 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -54,6 +55,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button5);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.textBox2);
@@ -63,38 +65,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(160, 449);
             this.panel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 74);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(24, 105);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(110, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(24, 167);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(110, 20);
-            this.textBox2.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 137);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
             // 
             // button1
             // 
@@ -106,28 +76,61 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(24, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(110, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
             // panel2
             // 
             this.panel2.Controls.Add(this.button3);
             this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.label3);
-            this.panel2.Location = new System.Drawing.Point(178, 363);
+            this.panel2.Location = new System.Drawing.Point(178, 12);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(610, 98);
+            this.panel2.Size = new System.Drawing.Size(610, 449);
             this.panel2.TabIndex = 4;
             // 
-            // label3
+            // button3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(114, 16);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "label3";
+            this.button3.Location = new System.Drawing.Point(261, 414);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 61);
+            this.button2.Location = new System.Drawing.Point(44, 414);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(172, 23);
             this.button2.TabIndex = 1;
@@ -135,15 +138,15 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // label3
             // 
-            this.button3.Location = new System.Drawing.Point(252, 61);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(174, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(92, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label3";
             // 
             // radioButton1
             // 
@@ -168,17 +171,17 @@
             this.panel3.Size = new System.Drawing.Size(391, 172);
             this.panel3.TabIndex = 7;
             // 
-            // radioButton2
+            // radioButton4
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton2.Location = new System.Drawing.Point(19, 102);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(101, 20);
-            this.radioButton2.TabIndex = 7;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton4.Location = new System.Drawing.Point(197, 102);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(101, 20);
+            this.radioButton4.TabIndex = 9;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
             // 
             // radioButton3
             // 
@@ -192,17 +195,17 @@
             this.radioButton3.Text = "radioButton3";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
-            // radioButton4
+            // radioButton2
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButton4.Location = new System.Drawing.Point(197, 102);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(101, 20);
-            this.radioButton4.TabIndex = 9;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "radioButton4";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(19, 102);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(101, 20);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
@@ -233,6 +236,16 @@
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(607, 345);
             this.panel4.TabIndex = 12;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(36, 265);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // TestForm
             // 
@@ -276,5 +289,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button5;
     }
 }
--- year_paper/WindowsFormsApp2/TestForm.cs
+++ year_paper/WindowsFormsApp2/TestForm.cs
@@ -34,10 +34,18 @@ namespace WindowsFormsApp2
             button2.Text = "Подивитись результати";
             button3.Text = "Повернутись на головну";
             button4.Text = "Далі";
+            button5.Text = "Подивитись результати";
             questions = new List<TestQuestion>();
             questions.Add(new TestQuestion("1. Маринка кідкинула гральний кубик, який має 6 граней, на кожній з яких цифра від 1 до 6. Яка ймовірність того, що число, яке випаде, буде парним?", "0.4", "0.3", "0.5", "0.6", 3));
             questions.Add(new TestQuestion("2. В пакетику знаходиться насіння моркви, огірків та помідор. Моркви — 72 насінини, огірків — 45, помідор — 83. Яка ймовірність того, що з навмання посадженої насінини виросте морква?", "0.36", "0.48", "0.72", "0.81", 1));
-            questions.Add(new TestQuestion("З повного набору доміно беруть два каменя. Яка ймовірність того, що другий камінь доміно не можна приставити до першого?", "7/18", "11/18", "0.5", "0.1", 2));
+            questions.Add(new TestQuestion("3. З повного набору доміно беруть два каменя. Яка ймовірність того, що другий камінь доміно не можна приставити до першого?", "7/18", "11/18", "0.5", "0.1", 2));
+            questions.Add(new TestQuestion("4. Формула для обчислення перестановок", "Pn = (1+n)!", "Pn = (n-2)!", "Pn = n!", "Pn = 1*2*...*(1-n)", 2));
+            questions.Add(new TestQuestion("5. Розміщення", "m-елементні вибірки з n-елементних множин, які відрізняються елементами або порядком елементів", "вибірки, які відрізняються порядком елементів", "m-елементні вибірки з n-елементних множин, які відрізняються елементами і порядком елементів", "множини, для яких порядок елементів не є істотним", 1));
+            questions.Add(new TestQuestion("6. ??? ", "Формула повної ймовірності", "Формула Байєса", "Формула Бернуллі", "Формула Ла-Гранжа", 1));
+            questions.Add(new TestQuestion("7. Із натуральних чисел від 1 до 30 учень навмання називає одне. Яка ймовірність того, що це число кратне числу 5?", "1/10", "1/6", "1/5", "1/4", 2));
+            questions.Add(new TestQuestion("8. Із літер, написаних на окремих картках, склали слово «математика». Потім ці картки поміняли місцями, перетасували та взяли навмання одну з них. Яка ймовірність того, що це буде картка з літерою А?", "0.1", "0.2", "0.3", "0.4", 3));
+            questions.Add(new TestQuestion("9. В автоматі містяться 100 пронумерованих жетонів (від 1 до 100). Знайдіть імовірність того, що номер навмання витягнутого жетона не має цифру 3.", "0.11", "0.38", "0.19", "0.81", 4));
+            questions.Add(new TestQuestion("10. Задано цифри 1, 2, 3, 4, 5, 6. Із них утворюють усі можливі шестицифрові числа, використовуючи кожну цифру тільки один раз. Знайдіть імовірність того, що взяте навмання одне з чисел ділитиметься на 5.", "1/720", "1/6", "1/80", "1/5", 2));
 
             if (!File.Exists(Global.pathToDefaultParticipantFile))
             {
@@ -58,13 +66,14 @@ namespace WindowsFormsApp2
                 panel1.Visible = false;
                 panel2.Visible = false;
                 panel4.Visible = true;
-               // timer
-               if (currentQuestion >= questions.Count)
+                // timer
+                if (currentQuestion >= questions.Count)
                 {
                     panel2.Visible = true;
                     panel1.Visible = false;
                     panel4.Visible = false;
                     label3.Text = "Ваш результат " + result + " балів";
+                    label3.Location = new Point(ActiveForm.Width / 4, ActiveForm.Height / 3);
                     Global.participants.Add(new TestParticipant(textBox1.Text, textBox2.Text, result, DateTime.Now.ToString()));
 
                     var json = JsonConvert.SerializeObject(Global.participants);
@@ -111,6 +120,12 @@ namespace WindowsFormsApp2
         }
 
         private void button2_Click(object sender, EventArgs e)
+        {
+            TestResultsForm testResultsForm = new TestResultsForm();
+            testResultsForm.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
         {
             TestResultsForm testResultsForm = new TestResultsForm();
             testResultsForm.Show();
